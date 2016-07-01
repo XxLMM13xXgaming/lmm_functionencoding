dceb9619295039c21f60506feb235652bfb7e357
Matt S <XxLMM13xXgaming@gmail.com>
2016-07-01 15:15:34 -0400

    TheFile

---
+++ lua/autorun/lmm_fe.lua
@@ -0,0 +1,25 @@
+----------------
+-- How to use --
+----------------
+inst = [[
+1.) put your function inside the functiontoencode brackets
+2.) Take out the -- by the functiontoencode and the 2 prints below it..
+3.) locate in console the numbers below 'HERE IS THE CODE: ' and copy
+4.) Place the numbers in the theencodedcode table brackets and place ,'s after each number except the last (also make sure there is no space inbetween this may take a while)
+5.) put -- at the start of each functon just like you found when you got the addon...
+6.) take out the --'s where you see theencodedcode, string.Implode and everything below that.. Now run the code.. make sure everything works...
+  NOTE: If there are errors it is either with the theencodedcode not being in the right format OR your function has a error!
+7.) In your addon's code put theencodedcode with the encoded code and string.Implode and below..
+
+Thanks!
+]]
+--------------
+-- The Code --
+--------------
+--theencodedcode = {}
+--functiontoencode = [[]]
+--print("HERE IS THE CODE: \n\n")
+--print(string.byte(functiontoencode,1,string.len(functiontoencode)))
+--string.Implode(" ", theencodedcode)
+--local runcode = string.char(unpack(theencodedcode))
+--RunString(runcode)
